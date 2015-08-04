da9ec30bbfaaf721f94e97086c8747ee9f0f9cfa
Light Inside <lightinsidexes@gmail.com>
2015-08-04 23:21:32 +0300

    Player - created

---
+++ src/com/lightinside/model/Player.java
@@ -0,0 +1,25 @@
+package com.lightinside.model;
+
+/**
+ * Created by tork on 04.08.2015.
+ */
+public class Player {
+
+    private final String name;
+
+    private final Figure figure;
+
+    public Player(final String name,
+                  final Figure figure) {
+        this.name = name;
+        this.figure = figure;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+}
